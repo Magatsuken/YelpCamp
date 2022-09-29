df0db14e57926b73c4dcbcf0491c78f5cb29aef4
Nicholas Truong <magatsuken@gmail.com>
2022-09-29 09:15:40 -0400

    Add image and description

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const campground = require('../models/campground');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 // Destruct to use :)
-const {descriptors, places} = require('./seedHelpers');
+const { descriptors, places } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -14,15 +14,19 @@ db.once('open', () => {
 });
 
 // Delete everything then make new campgrounds
-const seedDB = async() => {
+const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i <50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const random18 = Math.floor(Math.random() * 18);
         const random21 = Math.floor(Math.random() * 21);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${descriptors[random18]} ${places[random21]}`
+            title: `${descriptors[random18]} ${places[random21]}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum minus assumenda porro, esse totam officia harum sint veniam tenetur voluptate, eius, animi explicabo! Corporis, commodi ut eveniet facere consequuntur rerum.",
+            price: `${price}`
         })
         await camp.save();
     }
